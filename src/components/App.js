a68815e4983db2488356d7a15a20e03d4ac5346a
denis <denniiskim@gmail.com>
2023-11-19 08:32:09 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,13 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+function About(){
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
 
 
 function App() {
@@ -25,6 +32,8 @@ function App() {
       <NavBar />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
